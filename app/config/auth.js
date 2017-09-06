3ba0f7ed2aeb894cae1e26bcf3f3832ed36c3d93
James Calhoun <jim.calhoun@outlook.com>
2017-09-06 17:27:56 -0400

    remove googlebooks auth

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,9 +1,6 @@
 'use strict';
 
 module.exports = {
-	'googleBooksAuth': {
-		key: process.env.GOOGLE_BOOKS_KEY
-	},
 	'githubAuth': {
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
